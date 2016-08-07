f3db9b3b801af107d4453aa54d1b7aedb782d4f7
ragdoll <dambrosio.andry@gmail.com>
2016-08-07 11:18:46 +0200

    Iniziato progetto

---
+++ Assets/Scripts/Rng.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class Rng
+{
+    static System.Random prng;
+
+    static void SetSeed(string seed)
+    {
+        prng = new System.Random(seed.GetHashCode());
+    }
+
+    static int GetNumber(int min, int max)
+    {
+        if (prng == null)
+        {
+            Debug.LogError("ERROR: no pseudo random number generator set.");
+            return 0;
+        }
+        return prng.Next(min, max);
+    }
+}
