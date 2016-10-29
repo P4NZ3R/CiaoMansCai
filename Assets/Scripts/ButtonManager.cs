28dd906e67a8d9829592350a190809b2f0b4a6a5
ragdoll <dambrosio.andry@gmail.com>
2016-10-29 21:47:33 +0200

    aggiunti input base per il telefono

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    static ButtonManager instance;
+
+    public static ButtonManager Instance
+    {
+        get{ return instance; }
+        set
+        {
+            if (instance)
+                Destroy(instance);
+            instance = value;
+        }
+    }
+
+    public bool requestMovement1;
+    public bool requestMovement2;
+    GameObject canvas;
+    Text currentWeaponText;
+    int currentWeapon;
+
+    public int CurrentWeapon
+    {
+        get{ return currentWeapon; }
+        set
+        {
+            if (value > 2)
+                currentWeapon = 0;
+            else
+                currentWeapon = value;
+            currentWeaponText.text = (currentWeapon + 1).ToString();
+        }
+    }
+
+    void Start()
+    {
+        instance = this;
+        canvas = GameObject.Find("Canvas");
+        currentWeaponText = GameObject.Find("switchWeapon").transform.GetChild(0).GetComponent<Text>();
+    }
+
+    //    void Update()
+    //    {
+    //        if (Input.touchCount == 1 && canvas.activeSelf)
+    //            canvas.SetActive(false);
+    //        if (Input.touchCount != 1 && !canvas.activeSelf)
+    //            canvas.SetActive(true);
+    //    }
+
+    public void RequestMovement1(bool value)
+    {
+        requestMovement1 = value;
+    }
+
+    public void RequestMovement2(bool value)
+    {
+        requestMovement2 = value;
+    }
+
+    public void SwitchWeapon()
+    {
+        CurrentWeapon++;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,130 +2,167 @@
 
 public class Player : MonoBehaviour
 {
-	public GameGenerator gameGenerator;
-	public GameObject planet;
-	SpriteRenderer isActiveSign;
-	public int teamId;
-	public int playerId;
-	public bool isAlive = true;
-	Vector2 playerDirection;
-	float anglePos = 0;
-	float speedMovement = 3f;
-	float basicBulletForce = 1000f;
-	float shotgunBulletForce = 800f;
-	float grenadeLauncherBulletForce = 1100f;
-	[HideInInspector]public bool canShoot = true;
+    public GameGenerator gameGenerator;
+    public GameObject planet;
+    SpriteRenderer isActiveSign;
+    public int teamId;
+    public int playerId;
+    public bool isAlive = true;
+    float anglePos = 0;
+    float speedMovement = 3f;
+    float basicBulletForce = 1000f;
+    float shotgunBulletForce = 800f;
+    float grenadeLauncherBulletForce = 1100f;
+    Vector2 shootDirection;
+    //for mobile
+    Vector2 inputPosStart;
+    Vector2 inputPosEnd;
+    Vector2 inputPosActual;
+    [SerializeField]AnimationCurve forceProjectileMobile;
+    [HideInInspector]public bool canShoot = true;
 
-	delegate void ShootSomething();
 
-	ShootSomething shootSomething;
-	// Use this for initialization
-	void Start()
-	{
-		isActiveSign = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
+    delegate void ShootSomething();
+
+    ShootSomething shootSomething;
+    // Use this for initialization
+    void Start()
+    {
+        isActiveSign = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
 //        isActiveSign.gameObject.GetComponent<Animation>().;
-		RefreshPosition(Vector2.Angle(transform.position - planet.transform.position, Vector3.right));
-		planet.GetComponent<SpriteRenderer>().color = Color.white;
-		planet.GetComponent<SpriteRenderer>().sprite = GraphicGenerator.GetPlanetSprite(GameGenerator.GetTeam(teamId), planet.transform.lossyScale);
-		shootSomething = ShootBasic;
-	}
+        RefreshPosition(Vector2.Angle(transform.position - planet.transform.position, Vector3.right));
+        planet.GetComponent<SpriteRenderer>().color = Color.white;
+        planet.GetComponent<SpriteRenderer>().sprite = GraphicGenerator.GetPlanetSprite(GameGenerator.GetTeam(teamId), planet.transform.lossyScale);
+        shootSomething = ShootBasic;
+    }
 
-	void Update()
-	{
-		if (!isAlive || gameGenerator.activeTeam != teamId || GameGenerator.GetTeam(teamId).ActivePlayer != playerId || !canShoot)
-		{
-			isActiveSign.enabled = false;
-			return;
-		}
-		isActiveSign.enabled = true;
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-		{
-			RefreshPosition(anglePos + Time.deltaTime * speedMovement / planet.transform.localScale.x);
-		}
-		else
-		{
-			if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-			{
-				RefreshPosition(anglePos - Time.deltaTime * speedMovement / planet.transform.localScale.x);
-			}
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha1))
-		{
-			shootSomething = ShootBasic;
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha2))
-		{
-			shootSomething = ShootShotgun;
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha3))
-		{
-			shootSomething = ShootGrenadeLauncher;
-		}
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			shootSomething();
-		}
-	}
+    void Update()
+    {
+        if (!isAlive || gameGenerator.activeTeam != teamId || GameGenerator.GetTeam(teamId).ActivePlayer != playerId || !canShoot)
+        {
+            isActiveSign.enabled = false;
+            return;
+        }
+        isActiveSign.enabled = true;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || ButtonManager.Instance.requestMovement1)
+        {
+            RefreshPosition(anglePos + Time.deltaTime * speedMovement / planet.transform.localScale.x);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || ButtonManager.Instance.requestMovement2)
+            {
+                RefreshPosition(anglePos - Time.deltaTime * speedMovement / planet.transform.localScale.x);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ButtonManager.Instance.CurrentWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ButtonManager.Instance.CurrentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ButtonManager.Instance.CurrentWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shootSomething();
+        }
+        switch (ButtonManager.Instance.CurrentWeapon)
+        {
+            case 0:
+                shootSomething = ShootBasic;
+                break;
+            case 1:
+                shootSomething = ShootShotgun;
+                break;
+            case 2:
+                shootSomething = ShootGrenadeLauncher;
+                break;
+        }
+        //phone input
+        if (Input.touchCount == 1)
+        {
+            inputPosActual = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+                inputPosStart = inputPosActual;
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                inputPosEnd = inputPosActual;
+                shootDirection = inputPosStart - inputPosEnd;
+                shootDirection = shootDirection.normalized * forceProjectileMobile.Evaluate(shootDirection.magnitude / 5f);
+                if (shootDirection.magnitude > 0.2f)
+                    shootSomething();
+                
+            }
+            Debug.DrawLine(inputPosStart, inputPosActual, Color.grey);
+            Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), 6f * (inputPosStart - inputPosActual).normalized * forceProjectileMobile.Evaluate((inputPosStart - inputPosActual).magnitude / 5f), forceProjectileMobile.Evaluate((inputPosStart - inputPosActual).magnitude / 5f) > 0.2f ? Color.white : Color.red);
+//            Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), inputPosStart - inputPosActual, forceProjectileMobile.Evaluate((inputPosStart - inputPosActual).magnitude / 5f) > 0.2f ? Color.white : Color.red);
+        }
+    }
 
-	// Update is called once per frame
-	void FixedUpdate()
-	{
-		if (!isAlive)
-			return;
-		//ruota il player secondo la posizione sul pianeta
-		Vector3 vectorToTarget = planet.transform.position - transform.position;
-		float anglePos = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg + 90f;
-		Quaternion q = Quaternion.AngleAxis(anglePos, Vector3.forward);
-		transform.rotation = q;
-		playerDirection = transform.up;
-	}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isAlive)
+            return;
+        //ruota il player secondo la posizione sul pianeta
+        Vector3 vectorToTarget = planet.transform.position - transform.position;
+        float anglePos = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg + 90f;
+        Quaternion q = Quaternion.AngleAxis(anglePos, Vector3.forward);
+        transform.rotation = q;
+        shootDirection = transform.up;
+    }
 
-	void RefreshPosition(float angle)
-	{
-		if (angle == anglePos)
-			return;
-		if (anglePos > 360)
-			anglePos -= 360;
-		Vector3 newNormal = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
-		transform.position = planet.transform.position + (planet.transform.localScale.x / 2f + transform.localScale.y / 2f) * newNormal;
-		anglePos = angle;
-	}
+    void RefreshPosition(float angle)
+    {
+        if (angle == anglePos)
+            return;
+        if (anglePos > 360)
+            anglePos -= 360;
+        Vector3 newNormal = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+        transform.position = planet.transform.position + (planet.transform.localScale.x / 2f + transform.localScale.y / 2f) * newNormal;
+        anglePos = angle;
+    }
 
-	public void HitPlayer()
-	{
-		GameGenerator.GetTeam(teamId).players[playerId] = null;
-		Destroy(gameObject);
-	}
+    public void HitPlayer()
+    {
+        GameGenerator.GetTeam(teamId).players[playerId] = null;
+        Destroy(gameObject);
+    }
 
-	public void ShootBasic()
-	{
-		canShoot = false;
-		GameObject clone = Instantiate(GameGenerator.BasicBullet, transform.TransformPoint(Vector3.up * (transform.localScale.y / 1.5f)), Quaternion.identity) as GameObject;
-		GameGenerator.followedObject = clone.transform;
-		clone.GetComponent<Rigidbody2D>().AddForce(playerDirection * basicBulletForce);
-	}
+    public void ShootBasic()
+    {
+        canShoot = false;
+        GameObject clone = Instantiate(GameGenerator.BasicBullet, transform.TransformPoint(Vector3.up * (transform.localScale.y / 1.5f)), Quaternion.identity) as GameObject;
+        GameGenerator.followedObject = clone.transform;
+        clone.GetComponent<Rigidbody2D>().AddForce(shootDirection * basicBulletForce);
+    }
 
-	public void ShootShotgun()
-	{
-		canShoot = false;
-		const float spreadFactor = 0.4f;
-		const int numProjectile = 5;
-		for (int i = 0; i < numProjectile; i++)
-		{
-			GameObject clone = Instantiate(GameGenerator.ShotgunBullet, transform.TransformPoint(Vector3.up * (transform.localScale.y / 1.5f)), Quaternion.identity) as GameObject;
-			GameGenerator.followedObject = clone.transform;
-			Vector2 bulletDirection = Rng.ApplyInaccuracy(playerDirection, spreadFactor);
-			clone.GetComponent<Rigidbody2D>().AddForce(bulletDirection * shotgunBulletForce);
-		}
-	}
+    public void ShootShotgun()
+    {
+        canShoot = false;
+        const float spreadFactor = 0.4f;
+        const int numProjectile = 5;
+        for (int i = 0; i < numProjectile; i++)
+        {
+            GameObject clone = Instantiate(GameGenerator.ShotgunBullet, transform.TransformPoint(Vector3.up * (transform.localScale.y / 1.5f)), Quaternion.identity) as GameObject;
+            GameGenerator.followedObject = clone.transform;
+            Vector2 bulletDirection = Rng.ApplyInaccuracy(shootDirection, spreadFactor);
+            clone.GetComponent<Rigidbody2D>().AddForce(bulletDirection * shotgunBulletForce);
+        }
+    }
 
-	public void ShootGrenadeLauncher()
-	{
-		canShoot = false;
-		const float spreadFactor = 0.2f;
-		GameObject clone = Instantiate(GameGenerator.GrenadeLauncherBullet, transform.TransformPoint(Vector3.up * (transform.localScale.y / 1.2f)), Quaternion.identity) as GameObject;
-		GameGenerator.followedObject = clone.transform;
-		Vector2 bulletDirection = Rng.ApplyInaccuracy(playerDirection, spreadFactor);
-		clone.GetComponent<Rigidbody2D>().AddForce(bulletDirection * grenadeLauncherBulletForce);
-	}
+    public void ShootGrenadeLauncher()
+    {
+        canShoot = false;
+        const float spreadFactor = 0.2f;
+        GameObject clone = Instantiate(GameGenerator.GrenadeLauncherBullet, transform.TransformPoint(Vector3.up * (transform.localScale.y / 1.2f)), Quaternion.identity) as GameObject;
+        GameGenerator.followedObject = clone.transform;
+        Vector2 bulletDirection = Rng.ApplyInaccuracy(shootDirection, spreadFactor);
+        clone.GetComponent<Rigidbody2D>().AddForce(bulletDirection * grenadeLauncherBulletForce);
+    }
 }
